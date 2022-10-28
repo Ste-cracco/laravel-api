03e1d7280cd2fbcf4f50fdb140bad33ad100ef73
Ste-cracco <s.cracco@yahoo.it>
2022-10-28 16:03:06 +0200

    Errore 'front.js'

---
+++ resources/js/front.js
@@ -0,0 +1,13 @@
+window.Vue = require('vue');
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+//Importo App.vue
+import App from '.views/App';
+
+const app = new Vue({
+    el: '#app',
+    render: (h) => h(App)
+});
